1dcd1c99a384f4297af3b6d227372cbde174bed7
gmahima <mahimagangavarapu@gmail.com>
2020-12-25 17:21:03 +0530

    add different components for native, web

---
+++ components/MyComp.web.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+export default function MyComp () {
+    return (
+        <View><Text>hi from web</Text></View>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 // @generated: @expo/next-adapter@2.1.5
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-
+import MyComp from '../components/MyComp'
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+      <MyComp/>
     </View>
   )
 }
